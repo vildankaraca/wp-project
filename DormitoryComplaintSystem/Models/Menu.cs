d3269a8ba718428225afe5b746e43dd14df78b4d
Ayşenur Yılmaz <aysenuryl819@gmail.com>
2025-12-13 14:04:53 +0300

    update menu edit

--- DormitoryComplaintSystem/Models/Menu.cs
+++ DormitoryComplaintSystem/Models/Menu.cs
@@ -8,11 +8,24 @@ namespace DormitoryComplaintSystem.Models
         public int Id { get; set; }
 
         [DataType(DataType.Date)]
+        [Required(ErrorMessage="Date is required")]
+        [Display(Name="Date")]
         public DateTime Date { get; set; } // Hangi günün menüsü
 
-        public string Soup { get; set; }      
+        [Required(ErrorMessage="Soup is required")]
+        [Display(Name="Soup")]
+        public string Soup { get; set; }   
+
+        [Required(ErrorMessage="Main Dish is required")]
+        [Display(Name="Main Dish")]
         public string MainDish { get; set; }  
+
+        [Required(ErrorMessage="Side Dish is required")]
+        [Display(Name="Side Dish")]
         public string SideDish { get; set; }  
+
+        [Required(ErrorMessage="Dessert is required")]
+        [Display(Name="Dessert")]
         public string Dessert { get; set; }   
     }
 }
